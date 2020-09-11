f8609607264a856752dd5e238c1fd66dbc15869e
Tom ODay <tomoday91@gmail.com>
2020-09-11 22:15:33 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,9 +3,13 @@ def find_item_by_name_in_collection(name, collection)
   selected_item = {}
   collection.each do |index_n|
     key.each do |item, value|
+      if value == name
+      selected_item = index_n
+    end
 binding.pry
 end
 end
+selected_item
 end
 
 def consolidate_cart(cart)
