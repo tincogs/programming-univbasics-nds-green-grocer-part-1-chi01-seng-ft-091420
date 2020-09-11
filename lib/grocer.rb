781d603fb3573439c54842b9ab4276dc4e8c250c
Tom ODay <tomoday91@gmail.com>
2020-09-11 22:27:20 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,16 +1,16 @@
 require "pry"
 def find_item_by_name_in_collection(name, collection)
-  selected_item = {}
-  collection.each do |index_n|
-    index_n.each do |item, value|
-      if value == name
-      selected_item = index_n
-      return selected_item
-    else selected_item = nil
-      end
+    selected_item = {}
+    collection.each do |index_n|
+        index_n.each do |item, value|
+            if value == name
+            selected_item = index_n
+            return selected_item
+            else selected_item = nil
+            end
+        end
     end
-end
-selected_item
+    selected_item
 end
 
 def consolidate_cart(cart)
