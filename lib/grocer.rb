d12db97d9b33bbc9ea309fa008b68fb8bb548431
Tom ODay <tomoday91@gmail.com>
2020-09-11 22:13:09 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,7 @@
 require "pry"
 def find_item_by_name_in_collection(name, collection)
   selected_item = {}
-  collection.each do |key|
+  collection.each do |index_n|
     key.each do |item, value|
 binding.pry
 end
